9a576079cfb4f7247e9d677dbbdfb54945a82686
Andrea Szabo <szaboande93@gmail.com>
2022-01-29 18:06:48 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-      <footer>
-        Coded by Andrea Szabo and is{" "}
-        <a href="https://github.com/andi-szabo/weather-react" target="_blank">
-          open source on Github{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <WeatherSearch />
+        <footer>
+          Coded by Andrea Szabo and is{" "}
+          <a href="https://github.com/andi-szabo/weather-react" target="_blank">
+            open source on Github{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
